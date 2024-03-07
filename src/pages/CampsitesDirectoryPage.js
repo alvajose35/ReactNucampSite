3e20f33b7b05e51c0d46e4665ed69113db4266c5
Jose V <alvajose35@gmail.com>
2024-03-07 09:05:34 -0500

    selectCampsiteById useState hook

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { Container, Row, Col, Button } from "reactstrap";
 import CampsiteDetail from "../features/campsites/CampsiteDetail";
 import CampsitesList from "../features/campsites/CampsitesList";
-import { selectRandomCampsite } from "../features/campsites/campsitesSlice";
+import { selectRandomCampsite, selectCampsiteById } from "../features/campsites/campsitesSlice";
 
 const CampsiteDirectoryPage = () => {
 	// let selectedCampsite = selectRandomCampsite();
@@ -12,17 +12,19 @@ const CampsiteDirectoryPage = () => {
 	// 	console.log(selectedCampsite);
 	// };
 
-	const [selectedCampsite, randCamp] = useState(selectRandomCampsite());
+	// const [selectedCampsite, randCamp] = useState(selectRandomCampsite());
+	const [campsiteId, setCampsiteId] = useState(0);
+	const selectedCampsite = selectCampsiteById(campsiteId);		
 
 	return (
 		<Container>
-			<Button onClick={() => randCamp(selectRandomCampsite())}>
+			{/* <Button onClick={() => randCamp(selectRandomCampsite())}>
 				Select Random Campsite
-			</Button>
+			</Button> */}
 
 			<Row>
 				<Col sm='5' md='7'>
-					<CampsitesList/>
+					<CampsitesList setCampsiteId={setCampsiteId}/>
 				</Col>
 				<Col sm='7' md='5'>
 					<CampsiteDetail campsite={selectedCampsite}/>
