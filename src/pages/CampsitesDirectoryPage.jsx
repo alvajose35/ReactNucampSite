d90575c143b23afcf66c2909ca7811be193ed86b
Jose V <alvajose35@gmail.com>
2024-03-08 16:06:56 -0500

    CampsiteDetailPage with useParams()

--- src/pages/CampsitesDirectoryPage.jsx
+++ src/pages/CampsitesDirectoryPage.jsx
@@ -1,35 +1,10 @@
-import { useState } from "react";
-import { Container, Row, Col, Button } from "reactstrap";
-import CampsiteDetail from "../features/campsites/CampsiteDetail";
+import { Container } from "reactstrap";
 import CampsitesList from "../features/campsites/CampsitesList";
-import { selectRandomCampsite, selectCampsiteById } from "../features/campsites/campsitesSlice";
 
 const CampsiteDirectoryPage = () => {
-	// let selectedCampsite = selectRandomCampsite();
-
-	// const toggleCampsite = () => {
-	// 	selectedCampsite = selectRandomCampsite();
-	// 	console.log(selectedCampsite);
-	// };
-
-	// const [selectedCampsite, randCamp] = useState(selectRandomCampsite());
-	const [campsiteId, setCampsiteId] = useState(0);
-	const selectedCampsite = selectCampsiteById(campsiteId);		
-
 	return (
 		<Container>
-			{/* <Button onClick={() => randCamp(selectRandomCampsite())}>
-				Select Random Campsite
-			</Button> */}
-
-			<Row>
-				<Col sm='5' md='7'>
-					<CampsitesList setCampsiteId={setCampsiteId}/>
-				</Col>
-				<Col sm='7' md='5'>
-					<CampsiteDetail campsite={selectedCampsite}/>
-				</Col>
-			</Row>
+			<CampsitesList/>
 		</Container>
 	);
 };
